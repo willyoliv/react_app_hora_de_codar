0ad9fdfdc94ce5979f91578cbbf3ac8d6f7864ed
willyoliv <willyoliversilva@gmail.com>
2022-05-09 13:46:32 -0300

    Subindo alterações da aula #13

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import OutraLista from './components/OutraLista';
+
+function App() {
+  const meusItens = ['React', 'Vue', 'Angular'];
+  return (
+    <div className="App">
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens={[]}/>
+    </div>
+  );
+}
+
+export default App;
